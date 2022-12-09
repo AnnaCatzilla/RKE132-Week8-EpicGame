ff753664aabecb3a8d4d7abd0834f09a17e09dbe
Anna Kozlova <annakozlova@MacBook-Air-Anna.local>
2022-12-09 22:42:12 +0200

    add week8 code

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,29 @@
+﻿string filePath = @"/Users/annakozlova/projects/";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(filePath,heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(filePath, villainFile));
+
+
+//string[] heroes = { "Geralt", "Doctor Strange", "Severus Snape", "Bilbo Baggins" };
+//string[] villains = { "Vaas Montenegro", "John Seed", "Twisted Fate", "Joker", "Voldemort" };
+string[] weapon = { "magic wand", "plastic fork", "banana", "wooden sword", "rubber duck" };
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} with {villainWeapon} tries to take over the world!");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
+
+
